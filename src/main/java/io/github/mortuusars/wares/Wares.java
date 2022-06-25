843e4cae05f704d88c97ed924d9ed3c1b778c23a
mortuusars <mortuusars@gmail.com>
2022-06-25 03:53:15 +0300

    Initial commit

---
+++ src/main/java/io/github/mortuusars/wares/Wares.java
@@ -0,0 +1,17 @@
+package io.github.mortuusars.wares;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(Wares.MOD_ID)
+public class Wares
+{
+    public static final String MOD_ID = "wares";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Wares()
+    {
+
+    }
+}
